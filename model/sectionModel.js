df8deb35df32269feb1231e16aa219fe27973cb5
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2020-11-13 22:09:42 +0600

    missing some file

--- model/sectionModel.js
+++ model/sectionModel.js
@@ -2,8 +2,7 @@ import mongoose from 'mongoose';
 const sectionSchema = new mongoose.Schema({
     sectionName: { 
         type : String,
-        maxlength: [45, 'A course allocation have a less than or equal 45 students in one section'],
-        minength: [35, 'A course allocation have a more than or equal 35 students in one section']
+        required: [true,'A course allocation has a section name!']
     }
 })
 
